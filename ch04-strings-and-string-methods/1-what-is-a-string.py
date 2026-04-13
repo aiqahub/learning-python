2c192cb3edb83710982e8aff3112ea5f94e63e6d
aiqahub <aiqahub@gmail.com>
2026-04-13 19:34:09 +0530

    Q and A

--- ch04-strings-and-string-methods/1-what-is-a-string.py
+++ ch04-strings-and-string-methods/1-what-is-a-string.py
@@ -2,23 +2,24 @@
 # Solutions to review exercies
 
 
-# Exercise 1
+# Exercise 1   Print a string that uses double quotation marks inside the string.
 print('There are "double quotes" in this string.')
 
 
-# Exercise 2
+# Exercise 2  Print a string that uses an apostrophe inside the string.
 print("This string's got an apostrophe.")
 
 
-# Exercise 3
+# Exercise 3  Printastringthatspansmultiplelines, withwhitespacepreserved.
 print(
     """This string was written on multiple lines,
       and it displays across multiple lines"""
 )
 
 
-# Exercise 4
-print(
-    "This one-line string was written out \
-      using multiple lines"
+# Exercise 4  Print a string that is coded on multiple lines but displays on a single line.
+print("This one-line string was written out \
+  using multiple lines"
 )
+
+
